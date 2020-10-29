d96397d2a6cd071d1f501b94d7a29613ed106ba9
Thijn <thijnglas@gmail.com>
2020-10-29 16:27:49 +0100

    bestand toegevoegd

--- vluchtenlingmohab-keuzeverhaal.py
+++ vluchtenlingmohab-keuzeverhaal.py
@@ -5,38 +5,41 @@ import time
 import os
 
 #intro
-print("Hallo mijn naam is Thijn Glas en ik ga het verhaal vertellen van Mohab. er komt steeds een stukje tekst bij als u op ENTER klikt.")
+print("Hallo mijn naam is Thijn Glas en ik ga het verhaal vertellen van Mohab. er komt steeds een stukje tekst bij als u op ENTER klikt. Als je het originele verhaal wilt zonder keuzes kunt u Enter inklikken zonder iets in hoeven te vullen.")
 
 time.sleep (1)
 
 #voorafgaand het verhaal
 tekst1= "Het verhaal van Mohab Alkatmeh begint in Hama, de stad in Syrië waar hij opgroeide."
-tekst2= "Het is vroeg in de ochtend als de 17-jarige Mohab van huis naar school loopt. Het lijkt een dag zoals alle andere. Op één ding na: om 8 uur begint zijn laatste examen. Wanneer hij dat haalt, krijgt hij zijn diploma."
-tekst3= "Op weg naar school wordt hij staande gehouden door twee mannen die aan komen rijden op een motor. Er wordt gevraagd of Mohab zijn identiteitsbewijs wil laten zien. Na wat gebabbel in hun walkietalkie bepalen ze dat hij mee moet naar een militaire outpost."
-tekst4= "Hij legt uit dat zijn examen om 8 uur begint, en dat hij het hele jaar opnieuw moet doen als hij hem niet haalt. De situatie slaat om. Mohab voelt een pistool tegen zijn hoofd, krijgt boeien om zijn polsen en wordt geblinddoekt. "
-tekst5= "De mannen nemen hem mee naar een plek, door zijn blinddoek heeft hij geen idee waar. In een gebouw wordt hij meegesleurd naar een kelder."
-tekst6= "‘Who is your god!’, ‘Who is your god!’, ‘Assad!, Assad!’, klinkt het vanuit de kelder. De middelbare scholier ziet niets maar kan aan het geschreeuw horen dat mensen om hem heen in deze ruimte gemarteld worden."
-tekst7= "Mohab wordt op een stoel gezet, de gijzelnemers bellen zijn vader en moeder. De mannen willen geld van Mohab zijn ouders, een bedrag dat zijn ouders niet kunnen betalen. Het dringt tot hem door. Hij is gekidnapt door handlangers van “Assad”."
-tekst8= "Tien dagen lang houden zij hem vast. Totdat ze hem op een dag met de auto ergens naartoe brengen. Hij herkent het gebied, het is richting de begraafplaats waar zijn opa en oma liggen begraven. ‘Hier gaan ze me nu afmaken’, denkt hij in zichzelf."
-tekst9= "Maar dat gebeurt niet. De kidnappers gooien hem uit de rijdende auto. Hij valt op de grond, de mannen rijden door en keren niet meer terug."
+tekst2= "Het is vroeg in de ochtend als je van huis naar school loopt. Het lijkt een dag zoals alle andere. Op één ding na: om 8 uur begint je laatste examen. als je die haalt ben je geslaagd."
+tekst3= "Op weg naar school wordt je staande gehouden door twee mannen die aan komen rijden op een motor. Er wordt gevraagd of je jouw identiteitsbewijs wil laten zien. Na wat gebabbel in hun walkietalkie bepalen ze dat je mee moet naar een militaire outpost."
+tekst4= "je legt uit dat jouw examen om 8 uur begint, en dat je het hele jaar opnieuw moet doen als je die niet haalt. De situatie slaat om. in een keer voel je een pistool tegen jouw hoofd, krijgt boeien om je polsen en wordt geblinddoekt. "
+tekst5= "De mannen nemen je mee naar een plek, door je blinddoek heb jij geen idee waar. In een gebouw wordt je meegesleurd naar een kelder."
+tekst6= "‘Who is your god!’, ‘Who is your god!’, ‘Assad!, Assad!’, klinkt het vanuit de kelder. De middelbare scholier ziet niets maar kan aan het geschreeuw horen dat mensen om je heen in deze ruimte gemarteld worden."
+tekst7= "je wordt op een stoel gezet, de gijzelnemers bellen jouw vader en moeder. De mannen willen geld van Mohab zijn ouders, een bedrag dat zijn ouders niet kunnen betalen. Het dringt tot hem door. Hij is gekidnapt door handlangers van “Assad”."
+tekst8= "Tien dagen lang houden zij je vast. Totdat ze op een dag met de auto ergens naartoe brengen. je herkent het gebied, het is richting de begraafplaats waar je opa en oma liggen begraven. ‘Hier gaan ze me nu afmaken’"
+tekst9= "Maar dat gebeurt niet. De kidnappers gooien je uit de rijdende auto. je valt op de grond, de mannen rijden door en keren niet meer terug."
+#keuze1
 
 #het vluchten
-vtekst1= "Vanaf die dag is Mohab geen middelbare scholier meer maar een oorlogsvluchteling."
-vtekst2= "“Het geld dat mijn ouders kregen dankzij de verkoop van het huis, gaven zij aan mij mee om de reis te kunnen bekostigen. Zo kon ik bijvoorbeeld de taxichauffeur betalen die mij naar Libanon ‘smokkelde’”."
+vtekst1= "Vanaf die dag ben je geen middelbare scholier meer maar een oorlogsvluchteling."
+vtekst2= "“Het geld dat mijn ouders kregen dankzij de verkoop van het huis, gaven zij aan je mee om de reis te kunnen bekostigen. Zo kon je bijvoorbeeld de taxichauffeur betalen die je naar Libanon ‘smokkelde’”."
 vtekst3= "Eenmaal in Libanon moet de overstap naar Turkije worden gemaakt."
-vtekst4= "Een man biedt aan om Mohad en dertig anderen in een rubberen bootje te brengen in ruil voor geld. Eenmaal op de boot bleken dat 46 mensen te zijn. Mohab: “Niet zoals afgesproken, maar we gingen toch.”"
-vtekst5= "Mohab kan nietsSoms is de storm zo heftig dat mensen smeken om terug te keren. Ook ontsnapt het bootje een keer nipt aan de Turkse douane. Maar wonder boven wonder halen de 46 mensen de overkant. Vanuit Turkije gaat Mohab Europa in. zien tijdens de overtocht, het is donker. Wel hoort hij huilende baby’s en het donderen van de wolken. Het waren geluiden die Mohab nog elke dag herinnert. "
-vtekst6= "Soms is de storm zo heftig dat mensen smeken om terug te keren. Ook ontsnapt het bootje een keer nipt aan de Turkse douane. Maar wonder boven wonder halen de 46 mensen de overkant. Vanuit Turkije gaat Mohab Europa in."
+vtekst4= "Een man biedt aan om jou en dertig anderen in een rubberen bootje te brengen in ruil voor geld. Eenmaal op de boot bleken dat 46 mensen te zijn"
+#keuze2
+vtekst5= "wonder boven wonder halen de 46 mensen de overkant. Vanuit Turkije ga je Europa in."
 
 #toevallig in de NL belandt
-ntekst1= "Dat de Syriër in Nederland belandt, is eigenlijk puur toeval. In eerste instantie wil hij naar het Duitse Frankfurt. Maar dat plan gaat op het laatste moment niet door."
-ntekst2= "“Tijdens mijn tocht in Europa werd ik noodgedwongen opgevangen in een ziekenhuis in Oostenrijk. Ik was zwak en moest continu overgeven. Waarschijnlijk omdat ik iets met varken had gegeten, dat is mijn lichaam niet gewend.”"
-ntekst3= "Het ziekenhuis wil Mohab niet laten gaan, ondanks dat hij de trein om 8 uur ’s morgens (weer dat tijdstip!) moet halen naar Frankfurt. “Ik moest die trein hebben want ik had van mijn laatste geld een ticket voor deze rit gekocht”."
-ntekst4= "Uiteindelijk gaat hij op eigen houtje het ziekenhuis uit. “Al mijn spullen waren alleen niet meer op de plek waar ze eerst lagen. Hoe dan ook, ik snel sprintte naar het station… bleek ik toch te laat zijn.”"
-ntekst5= "Zonder spullen, zonder geld, in een land waar hij de taal niet spreekt. Mohab, die destijds nog helemaal geen Engels kon, is in Oostenrijk ten einde raad."
-ntekst6= "Terwijl de tranen over zijn wangen lopen komt er een man die Arabisch spreekt naar hem toe. Hij vertelt hem de volgende trein om 9 uur te nemen naar Amsterdam. In Nederland is volgens de man meer plek voor vluchtelingen dan in Duitsland, dat al twee miljoen mensen heeft opgenomen."
+ntekst1= "In eerste instantie wil je naar het Duitse Frankfurt. Maar dat plan gaat op het laatste moment niet door."
+ntekst2= "“Tijdens je tocht in Europa word je noodgedwongen opgevangen in een ziekenhuis in Oostenrijk. je bent zwak en moet continu overgeven. Waarschijnlijk omdat je iets met varken had gegeten, dat is je lichaam niet gewend.”"
+ntekst3= "Het ziekenhuis wil je niet laten gaan, ondanks dat je de trein om 8 uur ’s morgens (weer dat tijdstip!) moet halen naar Frankfurt. “Ik moest die trein hebben want ik had van mijn laatste geld een ticket voor deze rit gekocht”."
+#keuze3
+ntekst4= "Uiteindelijk ga je op eigen houtje het ziekenhuis uit. “Al mijn spullen waren alleen niet meer op de plek waar ze eerst lagen. Hoe dan ook, ik snel sprintte naar het station… bleek ik toch te laat zijn.”"
+ntekst5= "Zonder spullen, zonder geld, in een land waar je de taal niet spreekt."
+ntekst6= "Terwijl de tranen over je wangen lopen komt er een man die Arabisch spreekt naar je toe. Hij vertelt je de volgende trein om 9 uur te nemen naar Amsterdam. In Nederland is volgens de man meer plek voor vluchtelingen dan in Duitsland, dat al twee miljoen mensen heeft opgenomen."
 ntekst7= "“De man zei dat ik moest doen alsof ik sliep wanneer de conducteur om een ticket zou vragen. En als de controleur mij toch wakker maakte, moest ik zeggen dat mijn vriend – die even naar de wc is – de kaartjes heeft.”"
-ntekst8= "Mohab gebruikt uiteindelijk beide tips. De slaaptruc in Duitsland en later ook in gebrekkig Engels de vriend-naar-de-wc-truc  in Nederland. Zo komt hij in 2015 aan in het land waar hij nu woont."
+#keuze 4
+ntekst8= "je gebruikt uiteindelijk beide tips. De slaaptruc in Duitsland en later ook in gebrekkig Engels de vriend-naar-de-wc-truc  in Nederland. Zo komt je aan in Nederland het land waar je nu woont."
 
 itekst1= "Voor vluchtelingen is Nederland complex. Ook is er het taalverschil.de lange tijden van inburgeren drijven veel vluchtelingen tot wanhoop. Meer dan een jaar heeft Mohab moeten wachten in opvangcentra."
 itekst2= "“Ik wist in die opvangcentra niet waar ik aan toe was. Of ik mocht blijven en of mijn ouders hierheen konden komen. Ik besloot mezelf nuttig te maken en daarom – met behulp van een online tool – RefInfo te bouwen tijdens mijn verblijf in de opvangcentra. Dit is een app voor Android-toestellen.”"
@@ -71,7 +74,7 @@ print (tekst8)
 input()
 print (tekst9)
 
-input()
+#keuze1
 print (brk)
 keuzeAA = input ("je hebt nu een keuze. A = je probeert te vluchten zodat ze je niet meer kunnen lastig vallen of B = Je hoopt dat je nooit meer door ze word lastig gevallen.")
 
@@ -80,3 +83,89 @@ if (keuzeAA == "A"):
 elif (keuzeAA == "B"):
     print("Het gaat de eerste 2 weken goed, je leeft je leven alsof er niks aan de hand is. tot dat je weer na de 4de week was ontvoerd. Het was geen goede keuze om te blijven. je leeft de rest van je leven in een gevangenis. Je had dus beter kunnen vluchten.")
     quit()
+
+print(brk)
+
+input()
+print (vtekst1)
+
+input()
+print (vtekst2)
+
+input()
+print (vtekst3)
+
+input()
+print (vtekst4)
+
+#keuze2
+print(brk)
+
+keuzeBA= input ("je hebt nu een keuze. ga je mee met de boot (A) of ga je voor meer geld een andere boot nemen waar minder mensen opzitten(B)")
+if (keuzeBA == "A"):
+    print("je neemt de boot")
+elif(keuzeBA == "B"):
+    print("je bent opgelicht door degene die je zou over varen. je hebt niet genoeg geld om een ander iemand te vragen. je hebt geen keuze meer om te vluchten. je bent nu dakloos en heb geen geld meer.")
+    quit()
+
+print(brk)
+input()
+print (vtekst5)
+
+input()
+print (ntekst1)
+
+input()
+print (ntekst2)
+
+input()
+print (ntekst3)
+
+#keuze3
+print (brk)
+
+keuzeCA= input ("je hebt nu een keuze. blijf je in het ziekenhuis (A) of ga je stiekem het ziekenhuis verlaten zodat je naar de trein kan (B)?")
+if (keuzeCA == "A"):
+    print("je blijft in het ziekhuis. daar word je geholpen maar als je op het punt staat om te vertrekken komt de duoane om je op te halen. je word daar onderzocht en wordt naar een vluchtelingen centrum gebracht in Duitsland.")
+    quit()
+elif (keuzeCA == "B"):
+    print("je besluit te vertrekken.")
+
+print (brk)
+
+input()
+print (ntekst4)
+
+input()
+print (ntekst5)
+
+input()
+print (ntekst6)
+
+input()
+print (ntekst7)
+
+print(brk)
+keuzeDA= input ("je hebt nu een keuze. besluit je om de tips van die man te gebruiken en naar Nederland te gaan (A) of ga je naar een politie bureau voor hulp.")
+if (keuzeDA == "A"):
+    print("je pakt de trein.")
+elif(keuzeDA == "B"):
+    print("je gaat naar het bureau, je word geholpen maar niet zoals je wou. je word doorgestuurd naar de douane, daar word je wel geholpen. je verhuist tijdelijk naar Duitsland. je bent 2 jaar later officieel een burger in Duitsland")
+    quit()
+
+print(brk)
+
+input()
+print (ntekst8)
+
+input()
+print (itekst1)
+
+input()
+print (itekst2)
+
+input()
+print (itekst3)
+
+input()
+print("dit is het einde van het verhaal, gemaakt door Thijn Glas")
