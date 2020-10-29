f8ba5093cac485a5f0385797619edcb739d8e1f7
Thijn <thijnglas@gmail.com>
2020-10-29 11:28:27 +0100

    bestanden toegevoegd

--- microbit_program.py
+++ microbit_program.py
@@ -1,6 +1,37 @@
 from microbit import *
 import random
-
+#dobbelsteen
 while True:
     if accelerometer.was_gesture('shake'):
         display.scroll(random.randint(2, 12))
+
+
+#magic 8 ball
+answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it"
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+]
+
+while True:
+    display.show("8")
+    if input.button_is_pressed(Button.A):
+        display.clear()
+        display.scroll(random.choice(answers))
---
+++ vluchtenlingmohab-keuzeverhaal.py
@@ -0,0 +1,82 @@
+#keuze verhaal van Mohab
+
+#imports
+import time
+import os
+
+#intro
+print("Hallo mijn naam is Thijn Glas en ik ga het verhaal vertellen van Mohab. er komt steeds een stukje tekst bij als u op ENTER klikt.")
+
+time.sleep (1)
+
+#voorafgaand het verhaal
+tekst1= "Het verhaal van Mohab Alkatmeh begint in Hama, de stad in Syrië waar hij opgroeide."
+tekst2= "Het is vroeg in de ochtend als de 17-jarige Mohab van huis naar school loopt. Het lijkt een dag zoals alle andere. Op één ding na: om 8 uur begint zijn laatste examen. Wanneer hij dat haalt, krijgt hij zijn diploma."
+tekst3= "Op weg naar school wordt hij staande gehouden door twee mannen die aan komen rijden op een motor. Er wordt gevraagd of Mohab zijn identiteitsbewijs wil laten zien. Na wat gebabbel in hun walkietalkie bepalen ze dat hij mee moet naar een militaire outpost."
+tekst4= "Hij legt uit dat zijn examen om 8 uur begint, en dat hij het hele jaar opnieuw moet doen als hij hem niet haalt. De situatie slaat om. Mohab voelt een pistool tegen zijn hoofd, krijgt boeien om zijn polsen en wordt geblinddoekt. "
+tekst5= "De mannen nemen hem mee naar een plek, door zijn blinddoek heeft hij geen idee waar. In een gebouw wordt hij meegesleurd naar een kelder."
+tekst6= "‘Who is your god!’, ‘Who is your god!’, ‘Assad!, Assad!’, klinkt het vanuit de kelder. De middelbare scholier ziet niets maar kan aan het geschreeuw horen dat mensen om hem heen in deze ruimte gemarteld worden."
+tekst7= "Mohab wordt op een stoel gezet, de gijzelnemers bellen zijn vader en moeder. De mannen willen geld van Mohab zijn ouders, een bedrag dat zijn ouders niet kunnen betalen. Het dringt tot hem door. Hij is gekidnapt door handlangers van “Assad”."
+tekst8= "Tien dagen lang houden zij hem vast. Totdat ze hem op een dag met de auto ergens naartoe brengen. Hij herkent het gebied, het is richting de begraafplaats waar zijn opa en oma liggen begraven. ‘Hier gaan ze me nu afmaken’, denkt hij in zichzelf."
+tekst9= "Maar dat gebeurt niet. De kidnappers gooien hem uit de rijdende auto. Hij valt op de grond, de mannen rijden door en keren niet meer terug."
+
+#het vluchten
+vtekst1= "Vanaf die dag is Mohab geen middelbare scholier meer maar een oorlogsvluchteling."
+vtekst2= "“Het geld dat mijn ouders kregen dankzij de verkoop van het huis, gaven zij aan mij mee om de reis te kunnen bekostigen. Zo kon ik bijvoorbeeld de taxichauffeur betalen die mij naar Libanon ‘smokkelde’”."
+vtekst3= "Eenmaal in Libanon moet de overstap naar Turkije worden gemaakt."
+vtekst4= "Een man biedt aan om Mohad en dertig anderen in een rubberen bootje te brengen in ruil voor geld. Eenmaal op de boot bleken dat 46 mensen te zijn. Mohab: “Niet zoals afgesproken, maar we gingen toch.”"
+vtekst5= "Mohab kan nietsSoms is de storm zo heftig dat mensen smeken om terug te keren. Ook ontsnapt het bootje een keer nipt aan de Turkse douane. Maar wonder boven wonder halen de 46 mensen de overkant. Vanuit Turkije gaat Mohab Europa in. zien tijdens de overtocht, het is donker. Wel hoort hij huilende baby’s en het donderen van de wolken. Het waren geluiden die Mohab nog elke dag herinnert. "
+vtekst6= "Soms is de storm zo heftig dat mensen smeken om terug te keren. Ook ontsnapt het bootje een keer nipt aan de Turkse douane. Maar wonder boven wonder halen de 46 mensen de overkant. Vanuit Turkije gaat Mohab Europa in."
+
+#toevallig in de NL belandt
+ntekst1= "Dat de Syriër in Nederland belandt, is eigenlijk puur toeval. In eerste instantie wil hij naar het Duitse Frankfurt. Maar dat plan gaat op het laatste moment niet door."
+ntekst2= "“Tijdens mijn tocht in Europa werd ik noodgedwongen opgevangen in een ziekenhuis in Oostenrijk. Ik was zwak en moest continu overgeven. Waarschijnlijk omdat ik iets met varken had gegeten, dat is mijn lichaam niet gewend.”"
+ntekst3= "Het ziekenhuis wil Mohab niet laten gaan, ondanks dat hij de trein om 8 uur ’s morgens (weer dat tijdstip!) moet halen naar Frankfurt. “Ik moest die trein hebben want ik had van mijn laatste geld een ticket voor deze rit gekocht”."
+ntekst4= "Uiteindelijk gaat hij op eigen houtje het ziekenhuis uit. “Al mijn spullen waren alleen niet meer op de plek waar ze eerst lagen. Hoe dan ook, ik snel sprintte naar het station… bleek ik toch te laat zijn.”"
+ntekst5= "Zonder spullen, zonder geld, in een land waar hij de taal niet spreekt. Mohab, die destijds nog helemaal geen Engels kon, is in Oostenrijk ten einde raad."
+ntekst6= "Terwijl de tranen over zijn wangen lopen komt er een man die Arabisch spreekt naar hem toe. Hij vertelt hem de volgende trein om 9 uur te nemen naar Amsterdam. In Nederland is volgens de man meer plek voor vluchtelingen dan in Duitsland, dat al twee miljoen mensen heeft opgenomen."
+ntekst7= "“De man zei dat ik moest doen alsof ik sliep wanneer de conducteur om een ticket zou vragen. En als de controleur mij toch wakker maakte, moest ik zeggen dat mijn vriend – die even naar de wc is – de kaartjes heeft.”"
+ntekst8= "Mohab gebruikt uiteindelijk beide tips. De slaaptruc in Duitsland en later ook in gebrekkig Engels de vriend-naar-de-wc-truc  in Nederland. Zo komt hij in 2015 aan in het land waar hij nu woont."
+
+itekst1= "Voor vluchtelingen is Nederland complex. Ook is er het taalverschil.de lange tijden van inburgeren drijven veel vluchtelingen tot wanhoop. Meer dan een jaar heeft Mohab moeten wachten in opvangcentra."
+itekst2= "“Ik wist in die opvangcentra niet waar ik aan toe was. Of ik mocht blijven en of mijn ouders hierheen konden komen. Ik besloot mezelf nuttig te maken en daarom – met behulp van een online tool – RefInfo te bouwen tijdens mijn verblijf in de opvangcentra. Dit is een app voor Android-toestellen.”"
+itekst3= "het was een moeilijke tijd maar ik heb het gered. nu ben ik een burger van Nederland."
+
+brk= "______________________________________________________________________________________________________________"
+# verhaal begint
+input()
+print (tekst1)
+
+input()
+print (tekst2)
+
+input()
+print (tekst3)
+
+input()
+print (tekst4)
+
+input()
+print (tekst5)
+
+input()
+print (tekst6)
+
+input()
+print (tekst7)
+
+input()
+print (tekst8)
+
+input()
+print (tekst9)
+
+input()
+print (brk)
+keuzeAA = input ("je hebt nu een keuze. A = je probeert te vluchten zodat ze je niet meer kunnen lastig vallen of B = Je hoopt dat je nooit meer door ze word lastig gevallen.")
+
+if (keuzeAA == "A"):
+    print("je ouders verkopen hun huis zodat ze je genoeg geld meegeven zodat je kunt vluchten.")
+elif (keuzeAA == "B"):
+    print("Het gaat de eerste 2 weken goed, je leeft je leven alsof er niks aan de hand is. tot dat je weer na de 4de week was ontvoerd. Het was geen goede keuze om te blijven. je leeft de rest van je leven in een gevangenis. Je had dus beter kunnen vluchten.")
+    quit()
